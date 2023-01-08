5603411dd36ff06d5745f08374de6a2ea6139c0e
eladjmc <eladjmc88@gmail.com>
2023-01-08 11:26:17 +0200

    added task 1.2 variables

---
+++ 1.2 vars2/variables2.js
@@ -0,0 +1,78 @@
+const main = ()=> {
+    /*--Delete the wrong answers--
+1. Which of the following is/are strings?
+
+c) '4'
+
+2. Which of the following is/are numbers?
+a) 4
+b) 4.0
+d) -4
+
+}
+
+3. Which of the following is/are booleans?
+a) true
+b) false
+
+4. What is the result of 80 + 71.2?
+a) 151.2
+
+
+5. What is the result of "80" + 71.2?
+c) "8071.2"
+
+6. Does 100 + 30 produce a number or a string?
+a) number
+
+7. Does "100" + 30 produce a number or a string?
+b)string
+
+
+
+//------Submit your answers under the
+//question------//
+
+
+
+*/
+//create three different ways to declare variables
+var one=1;
+let two=2;
+const three=3;
+
+//declare a variable and reassign a value to it
+let x;
+x=1;
+
+//create a variable and after that assign a string
+//with its value being: He's got it!
+let y;
+y = "He's got it!";
+
+
+
+//1. create a variable and assign a value on how
+//much a restaurant bill is.
+const restaurantBill= 756.5;
+
+// 2. create a variable and assign a value on how
+// much tax they should pay.
+let taxForBill = 0.17;
+
+// 3. create a variable that will calculate the bill
+// * tax and its output would be: Your total bill is
+// <total bill> $.
+
+let billingMessage = 'Your total bill is:' + (taxForBill * restaurantBill);
+
+// Round the number 50.6 to its nearest integer
+console.log(Math.round(50.6));
+
+//Create a variable that is undefined
+let a;
+}
+
+
+
+main();
