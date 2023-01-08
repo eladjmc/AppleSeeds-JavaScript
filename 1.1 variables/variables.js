664bf520f4de07545caec60bec442e8285658aa8
eladjmc <eladjmc88@gmail.com>
2023-01-08 13:32:45 +0200

    added 21. task

--- 1.1 variables/variables.js
+++ 1.1 variables/variables.js
@@ -19,4 +19,3 @@ const main = ()=> {
 
 
 
-main();
---
+++ 2.1 js/2.1.js
@@ -0,0 +1,13 @@
+// "use strict";
+function countryInfo(country,population,capitalCity){
+    return country +' has '+ population + ' million  people and its capital city is ' +capitalCity;
+}
+
+var1=countryInfo('Israel', 9, 'Jerusalem')
+var2=countryInfo('USA', 300, 'Washington')
+var3=countryInfo('Spain', 47, 'Madrid')
+
+
+console.log(var1);
+console.log(var2);
+console.log(var3);
