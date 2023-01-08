02f37c0797140e7480ab953d7be09a48ccd03934
eladjmc <eladjmc88@gmail.com>
2023-01-08 11:02:00 +0200

    added task 1.1

---
+++ 1.1 variables/variables.js
@@ -0,0 +1,22 @@
+const main = ()=> {
+    const str= "sadsafsagasg"
+    const num = 1;
+    const obj ={la : "aa"};
+    const bool=true;
+    const nothing= null;
+    const notSet=undefined;
+    const sym = Symbol("foo");
+
+    console.log('str type is : ',typeof str);
+    console.log('num type is : ',typeof num);
+    console.log('obj type is : ',typeof obj);
+    console.log('bool type is : ',typeof bool);
+    console.log('nothing type is : ',typeof nothing);
+    console.log('notSet type is : ',typeof notSet);
+    console.log('sym type is : ',typeof sym);
+
+}
+
+
+
+main();
