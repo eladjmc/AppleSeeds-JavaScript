fa62e6fdf15de79afb031a6719d51fefcd5f8f3a
eladjmc <eladjmc88@gmail.com>
2023-01-09 11:51:19 +0200

    added 4.1 task

---
+++ 4.1 js/4.1.js
@@ -0,0 +1,12 @@
+let numSiblings = prompt('How many siblings do you have?');
+
+if(numSiblings == 1){
+    console.log('1 Sibling');
+}
+
+else if(numSiblings > 1){
+    console.log('More than 1 sibling');
+}
+ else {
+    console.log('No siblings');
+ }
