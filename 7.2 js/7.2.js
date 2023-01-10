08c3cdd0a9fc6e6694e705d4302a5ee712439f9e
eladjmc <eladjmc88@gmail.com>
2023-01-10 15:21:48 +0200

    added 7.2 task

---
+++ 7.2 js/7.2.js
@@ -0,0 +1,14 @@
+// you have this string:
+// const string = "The more you know, the more you
+// know that you don't know";
+// We want to get the index of the last occurrence of the word
+// “you”. It should give you 42.
+// You can solve this problem without the method indexOf. But
+// if you can manage to do it also with indexOf you will get a
+// cookie
+
+const string = "The more you know, the more you know that you don't know";
+
+console.log(string.lastIndexOf("you"));
+// you can run a loop with length of the string find
+//the last index of by advancing but lasIndexOf is more fun and I don't even like cookies
