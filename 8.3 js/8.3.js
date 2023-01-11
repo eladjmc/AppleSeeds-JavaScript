744ca3e8d4f2490027865efcfb47fb3b6a839932
eladjmc <eladjmc88@gmail.com>
2023-01-11 15:15:59 +0200

    finshed 8.3 task

--- 8.3 js/8.3.js
+++ 8.3 js/8.3.js
@@ -1,13 +1,34 @@
 const book = {
-    name: "Tales of demons and gods",
-    authorName : "Mad Snail",
-    yearOfPublish : "2014"
-  };
+  name: "Tales of demons and gods",
+  authorName: "Mad Snail",
+  yearOfPublish: "2014",
+};
 const bookUtil = {
-    name: "Against the gods",
-    authorName : "Turtle me",
-    yearOfPublish : "2015",
-    getFirstPublished : (book1,book2)=> book1.yearOfPublish > book2.yearOfPublish ? book2 : book1 
-  };
+  name: "Against the gods",
+  authorName: "Turtle me",
+  yearOfPublish: "2015",
+  getFirstPublished: (book1, book2) =>
+    book1.yearOfPublish > book2.yearOfPublish ? book2 : book1,
+  setNewEdition: (book, year) => {
+    book.yearOfPublish = year;
+  },
+  setLanguage: (book, lang) => {
+    book.language = lang;
+  },
 
-  console.log(bookUtil.getFirstPublished(book,bookUtil).name);
+  setTranslation: (book, lang, trans) => {
+    book.translation = { translator: trans, language: lang };
+  },
+  setPublisher: (book, pName, pLocation) => {
+    book.publisher = { name: pName, location: pLocation };
+  },
+  isSamePublisher: (book1, book2) =>
+    book1.publisher.name === book2.publisher.name &&
+    book1.publisher.location === book2.publisher.location,
+};
+
+
+console.log(book.publisher);
+bookUtil.setPublisher(book, "jacob", "israel");
+bookUtil.setPublisher(bookUtil, "jacob", "israel");
+console.log(bookUtil.isSamePublisher(bookUtil,book));
